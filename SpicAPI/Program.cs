6164e8bdb9e16273794fd94a1542a167d9f6751d
Abirami <harigabig@gmail.com>
2026-04-01 17:27:54 +0530

    changes in basic  program.cs

--- SpicAPI/Program.cs
+++ SpicAPI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Spic.Infrastructure.Data;
 using Spic.Infrastructure.Services;
