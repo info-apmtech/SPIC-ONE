b4e3ccd21c22b2cffbc4b03f9a3182242f6edaf5
Abirami <harigabig@gmail.com>
2026-04-02 17:43:20 +0530

    Login  page view created

---
+++ SPIC.MauiBlazorApp/SPIC.MauiBlazorApp.Shared/Services/LoginState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SPIC.MauiBlazorApp.Shared.Services
+{
+	public class LoginState
+	{
+		public bool IsBusy { get; set; }
+		public string? ErrorMessage { get; set; }
+	}
+}
---
+++ SPIC.MauiBlazorApp/SPIC.MauiBlazorApp.Shared/Services/PlatformService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SPIC.MauiBlazorApp.Shared.Services
+{
+	public class PlatformService
+	{
+		public bool IsWeb { get; set; }
+	}
+}
