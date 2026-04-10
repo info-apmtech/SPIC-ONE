14f3e7c1fcd000ec957c3b995f610fd1c8299ad1
AbdulApmtech <abdul@apmtechnologies.in>
2026-04-10 10:11:33 +0530

    login Api

---
+++ SPIC.Core/DTOs/AdminViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SPIC.Core.DTOs
+{
+    public class AdminViewModel 
+
+    {
+        public class LoginViewModel
+        {
+            public string Name { get; set; } = "";
+            public string Email { get; set; } = "";
+            public string Password { get; set; } = "";
+        }
+
+
+        public class LoginResponseModel
+        {
+            public string Token { get; set; }
+            public UserInfo User { get; set; }
+            public DateTime Expiration { get; set; }
+
+        }
+    }
+
+
+ 
+
+
+
+
+}
