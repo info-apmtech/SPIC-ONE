25260d186e5248126795b05b28d59ec93f013cf0
AbuApmTech <abu@apmtechnologies.in>
2026-04-30 16:39:07 +0530

    Datatable modified and reused

---
+++ SPIC.MauiBlazorApp/SPIC.MauiBlazorApp.Shared/Services/ExportColumn.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SPIC.MauiBlazorApp.Shared.Services
+{
+    public class ExportColumn<T>
+    {
+        public string Header { get; set; } = "";
+        public Func<T, string> ValueSelector { get; set; } = _ => "";
+    }
+}
