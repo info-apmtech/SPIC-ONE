cb05729a1fdf714133a89d4d5b2c5b438f842918
DhobikApmtech <dhobik@apmtechnologies.in>
2026-06-06 16:36:52 +0530

    Added RM, SM, and AVP credit limit fields in approval process Enabled decimal credit limit input support Display RM credit limit to SM during approval Display RM and SM credit limits to AVP during approval Persist role-wise credit limit values with approvals Optimized approval card UI and reduced unused space Preserved existing approval workflow and validations

---
+++ Spic.Infrastructure/Migrations/20260606100841_v2.6CreditLimitInDealerApproval.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Spic.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class v26CreditLimitInDealerApproval : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "AvpCreditLimit",
+                table: "DealerApprovalHistories",
+                type: "numeric",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "RmCreditLimit",
+                table: "DealerApprovalHistories",
+                type: "numeric",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "SmCreditLimit",
+                table: "DealerApprovalHistories",
+                type: "numeric",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvpCreditLimit",
+                table: "DealerApprovalHistories");
+
+            migrationBuilder.DropColumn(
+                name: "RmCreditLimit",
+                table: "DealerApprovalHistories");
+
+            migrationBuilder.DropColumn(
+                name: "SmCreditLimit",
+                table: "DealerApprovalHistories");
+        }
+    }
+}
