13e7ab99f0c914c3279e1c4184eb4f1b6408547f
AbuApmTech <abu@apmtechnologies.in>
2026-07-10 11:07:59 +0530

    Dealership pdf enclosures and Attached document preview fixed

--- SpicAPI/Program.cs
+++ SpicAPI/Program.cs
@@ -71,6 +71,20 @@ builder.Services.AddAuthentication(options =>
         NameClaimType = ClaimTypes.Name,
         ClockSkew = TimeSpan.Zero
     };
+
+    options.Events = new JwtBearerEvents
+    {
+        OnMessageReceived = context =>
+        {
+            var accessToken = context.Request.Query["access_token"];
+            var path = context.HttpContext.Request.Path;
+            if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/api/DealerFile/view"))
+            {
+                context.Token = accessToken;
+            }
+            return Task.CompletedTask;
+        }
+    };
 });
 
 builder.Services.AddAuthorization();
