3530c2f60d25b5417eab7726ddaf2c0ec3e2319e
Abirami <abirami@apmtechnologies.in>
2026-08-20 16:03:16 +0530

    Logistics, Register, Subdealer code updated

---
+++ Spic.Infrastructure/Migrations/20260820071331_v3r_additionalphonenoupdateinwarehouse.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Spic.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class v3r_additionalphonenoupdateinwarehouse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AdditionalPhoneNo",
+                table: "Warehouses",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdditionalPhoneNo",
+                table: "Warehouses");
+        }
+    }
+}
