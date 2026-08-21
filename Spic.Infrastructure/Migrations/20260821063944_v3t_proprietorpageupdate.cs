340d7d813be9e0f307cbb9f36beb627acf77b77f
Abirami <abirami@apmtechnologies.in>
2026-08-21 12:45:34 +0530

    Proprietor details updated in db

---
+++ Spic.Infrastructure/Migrations/20260821063944_v3t_proprietorpageupdate.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Spic.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class v3t_proprietorpageupdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EducationalQualification",
+                table: "PartnerFamilyDetails",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaritalStatus",
+                table: "PartnerFamilyDetails",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EducationalQualification",
+                table: "DealerOwnershipInfos",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EducationalQualification",
+                table: "PartnerFamilyDetails");
+
+            migrationBuilder.DropColumn(
+                name: "MaritalStatus",
+                table: "PartnerFamilyDetails");
+
+            migrationBuilder.DropColumn(
+                name: "EducationalQualification",
+                table: "DealerOwnershipInfos");
+        }
+    }
+}
