5060e6c2df4f7683a0a1c90ad1998673c2338cef
Abirami <abirami@apmtechnologies.in>
2026-09-05 18:27:56 +0530

    migration applied for guest house

--- Spic.Infrastructure/Migrations/20260905125621_v4c_GuestHouse.Designer.cs
+++ Spic.Infrastructure/Migrations/20260905125621_v4c_GuestHouse.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Spic.Infrastructure.Data;
@@ -11,9 +12,11 @@ using Spic.Infrastructure.Data;
 namespace Spic.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260905125621_v4c_GuestHouse")]
+    partial class v4c_GuestHouse
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
